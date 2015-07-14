66fd0f952304c61d76d3489daffff130c77b0387
Yavor Atov <yavor.atov@gmail.com>
2015-07-14 10:14:14 +0300

    Made controller handle unscoped tokens

--- controllers/root.js
+++ controllers/root.js
@@ -84,10 +84,12 @@ var Root = (function() {
             if (config.tokens_engine === 'keystone') {
                 req.headers['X-Nick-Name'] = user_info.token.user.id;
                 req.headers['X-Display-Name'] = user_info.token.user.name;
-                req.headers['X-Roles'] = user_info.token.roles.map(function (role) {
+                req.headers['X-Roles'] = (user_info.token.roles || []).map(function (role) {
                     return role.name;
                 }).join(',');
-                req.headers['X-Organizations'] = user_info.token.project.id;
+                if (user_info.token.project) {
+                    req.headers['X-Organizations'] = user_info.token.project.id;
+                }
             } else {
                 req.headers['X-Nick-Name'] = user_info.id;
                 req.headers['X-Display-Name'] = user_info.displayName;
